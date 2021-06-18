ad9ce002a920d9747a90672c19cab8285910f0b3
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-06-18 17:26:58 -0300

    Create template.js

---
+++ template.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+type Props = {};
+export default class App extends Component<props> {
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Olá Mundo</Text>
+
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+  // ESTILOS PARA APLICAR
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      
+    },
+
+    titulo:{
+      backgroundColor: '#696969',
+      textAlign:"center",
+      fontSize:42,
+      paddingTop:20,
+    },
+
+  });
+
