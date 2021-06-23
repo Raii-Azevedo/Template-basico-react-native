ab1a96c6f0c43d3b4123f803726886117f73bbe6
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-06-23 11:44:29 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { View, Text , StyleSheet } from "react-native";
+
+class App extends Component {
+
+  render(){
+    return (
+      <View> 
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+});
+
+export default App;
